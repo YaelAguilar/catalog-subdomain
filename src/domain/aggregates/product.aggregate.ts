bdfec60b99e6a4bd6bbc816ffd22783a74d7774d
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-06-06 04:39:05 -0600

    [UPDATE] new vo has been implemented on the aggregate "product"

--- src/domain/aggregates/product.aggregate.ts
+++ src/domain/aggregates/product.aggregate.ts
@@ -1,49 +1,51 @@
 import { Money } from "../value-objects/money.vo";
 import { SKU } from "../value-objects/product-sku.vo";
+import { ProductDescription } from "../value-objects/product-description.vo";
+import { randomUUID } from "crypto";
 
 export class Product {
-    readonly id: string;
-    public name: string;
-    public description: string;
-    public price: Money;
-    public sku: SKU;
-    public categoryId: string;
+  readonly id: string;
+  public name: string;
+  public description: ProductDescription;
+  public price: Money;
+  public sku: SKU;
+  public categoryId: string;
 
-    private constructor(
-        id: string,
-        name: string,
-        description: string,
-        price: Money,
-        sku: SKU,
-        categoryId: string
-    ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.sku = sku;
-        this.categoryId = categoryId;
-    }
-    
-    public static create(
-        id: string,
-        name: string,
-        description: string,
-        price: Money,
-        sku: SKU,
-        categoryId: string
-    ): Product {
-        if (!name || name.length < 3) {
-            throw new Error("Product name must be at least 3 characters long");
-        }
-        return new Product(id, name, description, price, sku, categoryId);
-    }
+  private constructor(
+    id: string,
+    name: string,
+    description: ProductDescription,
+    price: Money,
+    sku: SKU,
+    categoryId: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.sku = sku;
+    this.categoryId = categoryId;
+  }
 
-    public updatePrice(newPrice: Money): void {
-        if (newPrice.amount === this.price.amount && newPrice.currency === this.price.currency)
-        {
-            return; // Sin cambios
-        }
-        this.price = newPrice;
+  public static create(
+    name: string,
+    description: ProductDescription,
+    price: Money,
+    sku: SKU,
+    categoryId: string
+  ): Product {
+    if (!name || name.length < 3) {
+      throw new Error("Product name must be at least 3 characters long.");
     }
+    const id = randomUUID();
+    return new Product(id, name, description, price, sku, categoryId);
+  }
+
+  public updatePrice(newPrice: Money): void {
+    this.price = newPrice;
+  }
+
+  public changeDescription(newDescription: ProductDescription): void {
+    this.description = newDescription;
+  }
 }
