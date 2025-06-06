88ddaac57478b7903138a49f3b096052c689d429
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-06-06 04:17:41 -0600

    [ADD] product (aggregate)

---
+++ src/domain/aggregates/product.aggregate.ts
@@ -0,0 +1,49 @@
+import { Money } from "../value-objects/money.vo";
+import { SKU } from "../value-objects/sku.vo";
+
+export class Product {
+    readonly id: string;
+    public name: string;
+    public description: string;
+    public price: Money;
+    public sku: SKU;
+    public categoryId: string;
+
+    private constructor(
+        id: string,
+        name: string,
+        description: string,
+        price: Money,
+        sku: SKU,
+        categoryId: string
+    ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.sku = sku;
+        this.categoryId = categoryId;
+    }
+    
+    public static create(
+        id: string,
+        name: string,
+        description: string,
+        price: Money,
+        sku: SKU,
+        categoryId: string
+    ): Product {
+        if (!name || name.length < 3) {
+            throw new Error("Product name must be at least 3 characters long");
+        }
+        return new Product(id, name, description, price, sku, categoryId);
+    }
+
+    public updatePrice(newPrice: Money): void {
+        if (newPrice.amount === this.price.amount && newPrice.currency === this.price.currency)
+        {
+            return; // Sin cambios
+        }
+        this.price = newPrice;
+    }
+}
